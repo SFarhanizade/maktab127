ff1cd0404693bd83017b8a4594f96f9b02ffe877
saeid lak <saeidlak@iranjackdeveloper.com>
2025-02-14 11:52:58 +0330

    fix welcome sout

--- src/Service.java
+++ src/Service.java
@@ -1,7 +1,6 @@
 public class Service {
     public static void main(String[] args) {
-        System.out.println("Welcome to this application!");
-        System.out.println("ali");
+        System.out.println("ali,Welcome to this application!");
         var obj1 = method(1, 2, 4, 5, 6);
         var obj2 = new Object();
 
