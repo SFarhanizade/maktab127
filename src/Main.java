406e837bdabe924c33cda7f1b9e6f8fe669c2982
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2025-02-14 09:46:52 +0330

    version 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var obj1 = method(1,2,3,4,5,6);
+        var obj1 = method(1, 2, 4, 5, 6);
         var obj2 = new Object();
     }
 
