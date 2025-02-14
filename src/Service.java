6cc0630cee3cc95dbc18741cde64576ac1d0f323
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2025-02-14 11:33:49 +0330

    add welcome message

--- src/Service.java
+++ src/Service.java
@@ -1,5 +1,7 @@
 public class Service {
     public static void main(String[] args) {
+        System.out.println("Welcome to this application!");
+
         var obj1 = method(1, 2, 4, 5, 6);
         var obj2 = new Object();
 
