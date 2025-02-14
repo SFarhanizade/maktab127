de9b552552b601f6d83fb3d1115efa4f573cd440
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2025-02-14 10:22:34 +0330

    add register method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         var obj1 = method(1, 2, 4, 5, 6);
         var obj2 = new Object();
+
+        Test.register("");
     }
 
     private static Object method(Object... objects) {
